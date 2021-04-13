ba379cae7f330e16d53a9774e45cd3f49545aabd
fengling <redgra0315@163.com>
2021-04-13 23:59:39 +0800

    编写脚手架
    Signed-off-by: fengling <redgra0315@163.com>

--- go.mod
+++ go.mod
@@ -3,17 +3,23 @@ module github.com/fengling/project/log_demo
 go 1.13
 
 require (
+	github.com/fsnotify/fsnotify v1.4.9
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang/protobuf v1.5.1 // indirect
 	github.com/jmoiron/sqlx v1.3.3
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/onsi/ginkgo v1.16.1 // indirect
+	github.com/onsi/gomega v1.11.0 // indirect
+	github.com/spf13/viper v1.7.1
 	github.com/ugorji/go v1.2.4 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.16.0
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
 	golang.org/x/sys v0.0.0-20210326220804-49726bf1d181 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
---
+++ project/web_app/settings/settings.go
@@ -0,0 +1,75 @@
+package settings
+
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+// 全部变量，用来保存程序的所有配置信息
+var Conf = new(AppConfig)
+
+type AppConfig struct {
+	Name      string `mapstructure:"name"`
+	Mode      string `mapstructure:"mode"`
+	Version   string `mapstructure:"version"`
+	StartTime string `mapstructure:"start_time"`
+	MachineID int64  `mapstructure:"machine_id"`
+	Port      int    `mapstructure:"port"`
+
+	*LogConfig   `mapstructure:"log"`
+	*MySQLConfig `mapstructure:"mysql"`
+	*RedisConfig `mapstructure:"redis"`
+}
+
+type LogConfig struct {
+	Level     string `mapstructure:"level"`
+	FileName  string `mapstructure:"filename"`
+	MaxSize   int    `mapstructure:"maxsize"`
+	MaxAge    int    `mapstructure:"maxage"`
+	MaxBackup int    `mapstructure:"maxbackup"`
+}
+
+type MySQLConfig struct {
+	Host         string `mapstructure:"host"`
+	Port         int    `mapstructure:"port"`
+	User         string `mapstructure:"user"`
+	Password     string `mapstructure:"password"`
+	Dbname       string `mapstructure:"dbname"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+}
+
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Password string `mapstructure:"password"`
+	Db       int    `mapstructure:"db"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
+
+func Init() (err error) {
+	//viper.SetConfigFile("config")                                                // 指定配置文件
+	//viper.SetConfigType("yaml")                                                  // 指定配置文件类型
+	viper.SetConfigFile("/data/golang/src/github.com/fengling/project/web_app/config.yaml") // 指定查找配置文件的路径
+	// 读取配置信息
+	err = viper.ReadInConfig() // 读取配置信息
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		return
+	}
+	//　反序列到　Conf 变量
+	if err := viper.Unmarshal(Conf); err != nil {
+		fmt.Printf("viper.Unmarshal failed,err:%v\n", err)
+	}
+
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		zap.L().Warn("配置文件进行了改变")
+		if err := viper.Unmarshal(Conf); err != nil { // 读取配置信息失败
+			panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		}
+	})
+	return
+}
